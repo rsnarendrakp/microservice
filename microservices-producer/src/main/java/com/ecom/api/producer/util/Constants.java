22e1ec6884342fb9e85d4070174a9778f2c23d37
Narendra Pathak <95630480+rsnarendrakp@users.noreply.github.com>
2022-02-06 20:18:41 +0530

    Delete microservices-producer directory

--- microservices-producer/src/main/java/com/ecom/api/producer/util/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.ecom.api.producer.util;
-
-public class Constants {
-	
-    public static final String USER_ADDRESS_SEQUENCE_NAME = "user_address_sequence";
-    public static final String USER_SEQUENCE_NAME = "user_sequence";
-}
