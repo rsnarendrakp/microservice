66cc5e060d33cba3b814f72894794c4ce231e484
Narendra Pathak <95630480+rsnarendrakp@users.noreply.github.com>
2022-02-06 20:18:17 +0530

    Delete microservices-consumer directory

--- microservices-consumer/src/main/java/com/microservices/consumer/common/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.microservices.consumer.common;
-
-public class Constants {
-
-	public static final String PRODUCER_APP_NAME="RESTSERVICE";
-	public static final String BASE_API_URL="/api_v1/";
-}
