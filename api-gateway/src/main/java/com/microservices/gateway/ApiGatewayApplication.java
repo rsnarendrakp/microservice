e7bb66f6b3f304ce3661af47889c5ff30fc2653a
Narendra Pathak <95630480+rsnarendrakp@users.noreply.github.com>
2022-02-06 20:01:43 +0530

    Add files via upload

---
+++ api-gateway/src/main/java/com/microservices/gateway/ApiGatewayApplication.java
@@ -0,0 +1,33 @@
+package com.microservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+//@EnableFeignClients
+//@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	/**
+	 * Global filter
+	 */
+	/*@Bean
+	public GlobalFilter globalFilter() {
+		return (exchange, chain) -> {
+			System.out.println("First Global filter");
+			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+				System.out.println("Second Global filter");
+			}));
+		};
+  }*/
+}
